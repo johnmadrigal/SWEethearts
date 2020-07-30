43ffaba269521c6a7a9ffec2f2f26911fd0e083d
John Madrigal <johnmadrigal17@gmail.com>
2020-07-29 19:34:23 -0700

    logic to toggle navbar based on authstatus.loggedIn

--- src/pages/NavigateBar.jsx
+++ src/pages/NavigateBar.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
-const NavigateBar = () => (
+const NavigateBar = (props) => {
+  // const [links, setLinks] = useState([]);
+  const { authStatus } = props
+  // console.log(props)
+  let links = !authStatus.isLoggedIn ? ["Login", "Signup"] : ["Submit", "Profile", "Logout"];
+  const navLinks = links.map( (link) => (
+    <Link to={`/${link.toLowerCase()}`}>
+        <span className="nav-link" style={{ color: 'white' }}>
+          {(link === "Submit") ? "Submit Idea" : link}
+        </span>
+    </Link>
+  ))
+return (
   <Navbar bg="primary" variant="dark">
     {/* TODO: Point this href to `/explore` if User is authenticated */}
     <Link to="/">
@@ -11,10 +23,11 @@ const NavigateBar = () => (
 
     {/* Set class for Login and Signup button Nav item to `margin-left: auto;` */}
     <Nav className="ml-auto">
+      {navLinks}
       {/* TODO: Remove inline styling in favor of separate stylesheet */}
 
       {/* nav-link class mimics styling without nesting anchor tags */}
-      <Link to="/submit">
+      {/* <Link to="/submit">
         <span className="nav-link" style={{ color: 'white' }}>
           Submit Idea
         </span>
@@ -28,10 +41,10 @@ const NavigateBar = () => (
         <span className="nav-link" style={{ color: 'white' }}>
           Signup
         </span>
-      </Link>
+      </Link> */}
     </Nav>
   </Navbar>
-);
+)};
 
 // SAFE TO DELETE?
 // // Search Bar Component
